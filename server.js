3665d77e50f92f4390e8b782398f5c1df5e6ecfe
Gosols <teemu.a.kosonen@hotmail.com>
2020-11-24 12:40:53 +0200

    working REST API

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Ticket = require("./model");
+const bodyParser = require("body-parser");
+
+const router = express.Router();
+const PORT = process.env.PORT || 4000;
+const jsonParser = bodyParser.json();
+const urencodedParser = bodyParser.urlencoded({ extended: false });
+
+// middleware or some shit
+app.use("/tickets", router);
+
+// CORS handling
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET,POST, DELETE");
+    return res.status(200).json({});
+  }
+
+  next();
+});
+
+// handle requests to routes
+router.get("/", (_, res) => {
+  Ticket.find()
+    .exec()
+    .then((doc) => {
+      console.log(doc);
+      res.status(200).json(doc);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  Ticket.remove({ _id: id })
+    .exec()
+    .then((result) => {
+      res.status(200).json(result);
+    });
+});
+// post a new ticket..
+router.post("/", jsonParser, (req, res) => {
+  const ticket = new Ticket({
+    _id: mongoose.Types.ObjectId(),
+    text: req.body.text,
+    category: req.body.category,
+  });
+  ticket.save().then((res) => {
+    console.log(res);
+  });
+
+  res.status(200).json({
+    message: "ticket posting successful",
+    createdTicket: ticket,
+  });
+});
+
+mongoose.connect(
+  "mongodb+srv://kb-user:kosonen95@kb-cluster.gc0bz.mongodb.net/kanban-db?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  async () => {
+    console.log("Database connection established.");
+  }
+);
+
+app.listen(PORT, () => {
+  console.log("server running on port " + PORT);
+});
